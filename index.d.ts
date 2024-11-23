79a7cac5faf9b5c2aa4ccce081ff0550ad61f7c6
Idle <651462+reonZ@users.noreply.github.com>
2024-11-23 14:04:53 +0100

    export foundry-pf2e types

--- index.d.ts
+++ index.d.ts
@@ -5,3 +5,6 @@ export type * from "./src/types";
 export { DateTime } from "luxon";
 export * as R from "remeda";
 export * from "./dist";
+
+export type * from "./exports";
+export type * from "foundry-pf2e/foundry/client-esm/applications/_types.d.ts";
