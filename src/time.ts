0fc4f4c9697b63ab56b8450fd4394bf896729847
Idle <651462+reonZ@users.noreply.github.com>
2024-12-01 14:07:45 +0100

    fix getShortDateTime date year when not using CE

--- src/time.ts
+++ src/time.ts
@@ -5,7 +5,15 @@ function getShortDateTime() {
     const worldClock = game.pf2e.worldClock;
     const worldTime = worldClock.worldTime;
     const time = getShortTime(worldTime);
-    const date = worldTime.toLocaleString(DateTime.DATE_SHORT);
+
+    const date =
+        worldClock.dateTheme === "CE"
+            ? worldTime.toLocaleString(DateTime.DATE_SHORT)
+            : DateTime.local(
+                  /** private */ worldClock["year"],
+                  worldTime.month,
+                  worldTime.day
+              ).toLocaleString(DateTime.DATE_SHORT);
 
     return {
         worldClock,
