a6eb09741a3174d0c8ebeaefcd32289a557535c1
Idle <651462+reonZ@users.noreply.github.com>
2025-04-04 17:18:28 +0200

    use GamePF2e interface

--- src/types/game.d.ts
+++ src/types/game.d.ts
@@ -1,16 +1,7 @@
 export {};
 
-declare global {
-    interface Game<
-        TActor extends Actor<null>,
-        TActors extends Actors<TActor>,
-        TChatMessage extends ChatMessage,
-        TCombat extends Combat,
-        TItem extends Item<null>,
-        TMacro extends Macro,
-        TScene extends Scene,
-        TUser extends User<TActor>
-    > {
+declare module "foundry-pf2e" {
+    interface GamePF2e {
         toolbelt?: {
             getToolSetting: <
                 TName extends keyof toolbelt.settings,
@@ -31,5 +22,3 @@ declare global {
         };
     }
 }
-
-declare module "foundry-pf2e/types/foundry/client/game.js" {}
