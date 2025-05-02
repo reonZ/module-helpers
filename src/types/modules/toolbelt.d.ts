729f985fa164bc7277a2fc8b7ba26837bdd5f8ef
Idle <651462+reonZ@users.noreply.github.com>
2025-05-02 12:28:36 +0200

    add some toolbelt types

---
+++ src/types/modules/toolbelt.d.ts
@@ -0,0 +1,50 @@
+import {
+    DegreeAdjustmentsRecord,
+    DegreeOfSuccessString,
+    RollNoteSource,
+    SaveType,
+} from "foundry-pf2e";
+
+declare global {
+    namespace toolbelt {
+        namespace targetHelper {
+            type TargetMessageType = "damage" | "spell-damage" | "spell-save" | "action" | "check";
+
+            type MessageTargetSave = {
+                private: boolean;
+                value: number;
+                die: number;
+                success: DegreeOfSuccessString;
+                roll: string;
+                notes: RollNoteSource[];
+                dosAdjustments: DegreeAdjustmentsRecord | undefined;
+                unadjustedOutcome?: DegreeOfSuccessString | null;
+                modifiers: { label: string; modifier: number }[];
+                significantModifiers: modifiersMatter.SignificantModifier[] | undefined;
+                rerolled?: "hero" | "new" | "lower" | "higher";
+                statistic: SaveType;
+            };
+
+            type MessageFlag = {
+                type?: TargetMessageType;
+                targets?: string[];
+                save?: MessageSaveFlag;
+                saves?: Record<string, MessageTargetSave>;
+                splashIndex?: number;
+                isRegen?: boolean;
+                applied?: Record<string, boolean[]>;
+                options?: string[];
+                traits?: string[];
+                item?: ItemUUID;
+                splashTargets?: string[];
+            };
+
+            type MessageSaveFlag = {
+                statistic: SaveType;
+                basic: boolean;
+                dc: number;
+                author?: string;
+            };
+        }
+    }
+}
