aed1b65d33a3ac5defd8d1eecce331278b320cfb
Idle <651462+reonZ@users.noreply.github.com>
2025-06-06 17:34:13 +0200

    add getTranslatedSkills helper

--- dist/skill.d.ts
+++ dist/skill.d.ts
@@ -1,3 +1,4 @@
 import { SkillSlug } from "foundry-pf2e";
+declare function getTranslatedSkills(lowercase?: boolean): Record<"athletics" | "deception" | "stealth" | "nature" | "acrobatics" | "arcana" | "crafting" | "diplomacy" | "intimidation" | "medicine" | "occultism" | "performance" | "religion" | "society" | "survival" | "thievery", string>;
 declare function getSkillLabel(skill: SkillSlug, localize?: boolean): string;
-export { getSkillLabel };
+export { getSkillLabel, getTranslatedSkills };
