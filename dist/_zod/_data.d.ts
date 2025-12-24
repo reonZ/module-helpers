85fe368087ebb85caf75ddb0fce82b2ca95bb296
Idle <651462+reonZ@users.noreply.github.com>
2025-12-24 21:48:40 +0100

    use direct zod schemas instead of functions

--- dist/_zod/_data.d.ts
+++ dist/_zod/_data.d.ts
@@ -1,11 +1,8 @@
 import { z } from "..";
-declare function zID(): z.ZodReadonly<z.ZodDefault<z.ZodString>>;
-declare function zPosition(): z.ZodDefault<z.ZodObject<{
+declare const zID: z.ZodReadonly<z.ZodDefault<z.ZodString>>;
+declare const zPosition: z.ZodDefault<z.ZodObject<{
     x: z.ZodDefault<z.ZodNumber>;
     y: z.ZodDefault<z.ZodNumber>;
 }, z.z.core.$strip>>;
-/**
- * `z.string().trim().min(1)`
- */
-declare function zString(): z.ZodString;
+declare const zString: z.ZodString;
 export { zID, zPosition, zString };
--- src/_zod/_data.ts
+++ src/_zod/_data.ts
@@ -1,23 +1,14 @@
 import { z } from "..";
 
-function zID() {
-    return z.string().trim().length(16).default(foundry.utils.randomID).readonly();
-}
+const zID = z.string().trim().length(16).default(foundry.utils.randomID).readonly();
 
-function zPosition() {
-    return z
-        .object({
-            x: z.number().default(0),
-            y: z.number().default(0),
-        })
-        .default({ x: 0, y: 0 });
-}
+const zPosition = z
+    .object({
+        x: z.number().default(0),
+        y: z.number().default(0),
+    })
+    .default({ x: 0, y: 0 });
 
-/**
- * `z.string().trim().min(1)`
- */
-function zString() {
-    return z.string().trim().min(1);
-}
+const zString = z.string().trim().min(1);
 
 export { zID, zPosition, zString };
