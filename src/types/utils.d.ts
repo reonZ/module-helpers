29a1b81a4722e804ebd8c6c238d64710b86ec14a
Idle <651462+reonZ@users.noreply.github.com>
2025-12-27 12:25:25 +0100

    small tweak on SelectOption types

--- src/types/utils.d.ts
+++ src/types/utils.d.ts
@@ -22,9 +22,9 @@ declare global {
 
     type SelectOption<V extends string = string> = { value: V; label?: string };
 
-    type SelectOptions<V extends string = string> = SelectOption<V>[];
+    type SelectOptions<V extends string = string> = { value: V; label?: string }[];
 
-    type RequiredSelectOptions<V extends string = string> = Required<SelectOption<V>>[];
+    type RequiredSelectOptions<V extends string = string> = { value: V; label: string }[];
 
     type Merge<T extends object> = {
         [k in T extends any ? keyof T : never]?: T extends { [l in k]?: infer V } ? V : never;
